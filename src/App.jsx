4cb763ba6a07f1da1bf03148daa263297845a6f7
amirhosein sajjadi <canadavancouver2003@gmail.com>
2025-02-05 16:01:38 +0330

    add jsx items

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { StrictMode } from 'react'
+import Calculator from "./components/Calculator";
+import './App.css'
+
+
+function App() {
+  return(
+    <>
+      <Calculator></Calculator>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Calculator.jsx
@@ -0,0 +1,30 @@
+
+function Calculator(){
+    return(
+        <>
+            <div className="container">
+                <div className="screen"></div>
+                <div className="buttons">
+                    <button>Clear</button>
+                    <button>C</button>
+                    <button>÷</button>
+                    <button>7</button>
+                    <button>8</button>
+                    <button>9</button>
+                    <button>×</button>
+                    <button>4</button>
+                    <button>5</button>
+                    <button>6</button>
+                    <button>-</button>
+                    <button>1</button>
+                    <button>2</button>
+                    <button>3</button>
+                    <button>.</button>
+                    <button>=</button>
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default Calculator ;
